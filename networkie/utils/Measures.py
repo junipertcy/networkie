bdd5548d0217daa01591f17d2d38c0e87978c957
Roytsai27 <38278579+Roytsai27@users.noreply.github.com>
2018-04-30 11:49:10 +0800

    Finish!

--- networkie/utils/Measures.py
+++ networkie/utils/Measures.py
@@ -1,8 +1,16 @@
-def compute_num_triangles():  # This is Prob. 3-e.
+def compute_num_triangles(G):  # This is Prob. 3-e.
     '''
     Write your code documentation here.  # This is Prob. 4-a.
     '''
-    return
+    def nodes_connected(u, v):
+    	return u in G.neighbors(v)
+    t= 0
+    for i in  G.nodes():
+    	neighbors = list(G.neighbors(i))
+    	pairs = [(i,v) for i in neighbors for v in neighbors if i!=v]
+    	for u,s in pairs:
+    		if nodes_connected(u,s):t+=1
+    return t/6
 
 
 class Node(object):
