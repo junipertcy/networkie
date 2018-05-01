c37f646a55914666b48024dc5df2d4f0813486e2
meizhengzheng <meidin138@gmail.com>
2018-05-01 23:54:55 +0800

    hw3

--- networkie/gen/Custom.py
+++ networkie/gen/Custom.py
@@ -1,6 +1,5 @@
 import networkx as nx
-
-
+import pandas as pd
 class LoadFromFile(object):
     def __init__(self):
         """
@@ -36,8 +35,41 @@ class LoadFromFile(object):
         print('Edgelist txt data successfully loaded into a networkx Graph!')
         return self.g
 
-    def from_in_class_network(self):  # This is Prob. 3-a.
+    def from_in_class_network(self,r):  # This is Prob. 3-a.
         '''
         Write your code documentation here.  # This is Prob. 4-a.
+         
+         Parameters
+        ----------
+        r: file name
+            use pandas to read a file 
+            take two lines of information ['ID'] , ['IDs-of-acquaintances']
+            for the value of ['IDs-of-acquaintances'] change from 'str' to 'int'
+            add all number that exist in the value of ['ID'] and ['IDs-of-acquaintances']
+       
+        Returns
+        ----------
         '''
+        df = pd.read_csv('In-class-network.txt',delimiter = '\t')
+        ID = df['ID']
+        a = df['IDs-of-acquaintances']
+        
+        for i in range(len(a)):
+            if "," in a[i]:
+                a[i] = a[i].split(",")
+                for j in range(len(a[i])):
+                    a[i][j] = int(a[i][j])
+            elif a[i] == ' ':
+                continue
+            else:
+                a[i] = [int(a[i])]
+
+            self.g.add_nodes_from(a[i])
+            self.g.add_nodes_from(ID)
+
+            for j in range(len(a[i])):
+                self.g.add_edge(int(ID[i]),a[i][j])
+        
         return self.g
+
+
--- networkie/utils/Measures.py
+++ networkie/utils/Measures.py
@@ -1,7 +1,18 @@
-def compute_num_triangles():  # This is Prob. 3-e.
+def compute_num_triangles(g):  # This is Prob. 3-e.
     '''
     Write your code documentation here.  # This is Prob. 4-a.
+    
+    Parameters
+    ----------
+    count how many triangles for each node and sum all triangles ,because it count repeatly therefore then divid by 3
+
+    Returns
+    -------
+    g: `NetworkX graph`
     '''
+    t = nx.triangles(g)
+    return print('number of triangles :',sum(t.values())/3)
+
     return
 
 
@@ -15,6 +26,15 @@ class Node(object):
     def degree_dist(self):  # This is Prob. 3-d.
         '''
         Write your code documentation here.  # This is Prob. 4-a.
+
+        Parameters
+        ----------
+        put every degree into list
+    
         '''
-        return list()
+        degree = []
+        for i in range((G.order())):
+            degree.append(G.degree(i))
+            
+        return degree()
 
