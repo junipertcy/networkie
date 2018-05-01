a695b8cf8f8c05152a50979b5ff2520258443749
qwertyannie <38278681+qwertyannie@users.noreply.github.com>
2018-05-01 21:40:14 +0800

    Update Custom.py

--- networkie/gen/Custom.py
+++ networkie/gen/Custom.py
@@ -36,8 +36,43 @@ class LoadFromFile(object):
         print('Edgelist txt data successfully loaded into a networkx Graph!')
         return self.g
 
-    def from_in_class_network(self):  # This is Prob. 3-a.
+    def from_in_class_network(self, path):  # This is Prob. 3-a.
         '''
         Write your code documentation here.  # This is Prob. 4-a.
+        
+        Parameters
+        ----------
+        path: `str`
+            The path to the edgelist text file.
+        
+        Returns
+        -------
+        G: `NetworkX graph`
+            The parsed graph.
+        
         '''
+        df = pd.read_table(path)
+        n = []
+        for i in range(df["ID"].shape[0]):
+            n.append(df["ID"][i])
+        L = []
+        for i in list(df["IDs-of-acquaintances"]):
+            i = i.split(",")
+            L.append(i)
+        for i in range(len(L)):
+            for j in range(len(L[i])):
+                if L[i][j] != " ":
+                    L[i][j] = int(L[i][j])
+        for i in range(len(L)):
+            for j in range(len(L[i])):
+                if L[i][j] != " " and L[i][j] not in n:
+                    n.append(L[i][j])
+        self.g.add_nodes_from(n)
+        e = []
+        for i in range(len(L)):
+            if ' ' not in L[i]:
+                for j in range(len(L[i])):
+                    e.append((df["ID"][i], L[i][j]))
+        self.g.add_edges_from(e)
+                
         return self.g
