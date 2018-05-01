bc5ac03eec81ac95abf2ad88a90db81a53332643
sally <sally990482@gmail.com>
2018-05-01 08:18:24 +0800

    submit

--- networkie/gen/Custom.py
+++ networkie/gen/Custom.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import networkx as nx
-
+import numpy as np
+import pandas as pd
 
 class LoadFromFile(object):
     def __init__(self):
@@ -36,8 +38,44 @@ class LoadFromFile(object):
         print('Edgelist txt data successfully loaded into a networkx Graph!')
         return self.g
 
-    def from_in_class_network(self):  # This is Prob. 3-a.
+    def from_in_class_network(self,path):  # This is Prob. 3-a.
         '''
         Write your code documentation here.  # This is Prob. 4-a.
+        
+        Parameters
+        ----------
+        path: `file`
+            The path is about what data you want to know.
+        Returns
+        -------
+        G: `NetworkX graph`
+            The parsed graph.
         '''
-        return self.g
+        one = []
+        with open(path,"r") as f:
+            for line in f:
+                line = line.strip().split("\t")
+                one.append(line)
+        one=pd.DataFrame(one[1:],columns=one[0])
+        
+        ID=[]
+        ID_node=[]
+        for i in range(len(one)):
+            if one["IDs-of-acquaintances"][i]==" ":
+                ID_node.append(int(one["ID"][i]))
+            if one["IDs-of-acquaintances"][i]!=" ":    
+                ID.append(one["ID"][i])
+                ID.append(one["IDs-of-acquaintances"][i])
+
+        for i in range(1,len(ID),2):
+            ID[i]=ID[i].split(",")
+
+        number=[]
+        for j in range(1,len(ID),2):
+            for k in range(len(ID[j])):
+                number.append((int(ID[j-1]),int(ID[j][k])))
+
+        self.g=nx.Graph()
+        self.g.add_nodes_from(ID_node)
+        self.g.add_edges_from(number)
+        return self.g 
--- networkie/utils/Measures.py
+++ networkie/utils/Measures.py
@@ -1,8 +1,23 @@
-def compute_num_triangles():  # This is Prob. 3-e.
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+import pandas as pd
+def compute_num_triangles(g):  # This is Prob. 3-e.
     '''
     Write your code documentation here.  # This is Prob. 4-a.
+        Parameters
+        ----------
+        g: `graph`
+            The graph is related about data.
+
+        Returns
+        -------
+        total: total triangles
     '''
-    return
+
+    tri = nx.triangles(g)
+    total=sum(tri.values())/3
+    return total
 
 
 class Node(object):
@@ -12,9 +27,20 @@ class Node(object):
     def betweenness(self):
         pass
 
-    def degree_dist(self):  # This is Prob. 3-d.
+    def degree_dist(self,g):  # This is Prob. 3-d.
         '''
         Write your code documentation here.  # This is Prob. 4-a.
+        Parameters
+        ----------
+        g: `graph`
+            The graph is related about data.
+
+        Returns
+        -------
+        g_degree: every id's degree
         '''
-        return list()
+        g_degree=[]
+        for i in range(len(g.degree())):
+            g_degree.append(g.degree()[i])
+        return g_degree
 
