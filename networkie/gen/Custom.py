ada5c3e67941f19b7852b4afbdade47162a83c97
gyuler83821 <gyuler83821@yahoo.com.tw>
2018-05-01 23:14:01 +0800

    finish by E44066034

--- networkie/gen/Custom.py
+++ networkie/gen/Custom.py
@@ -1,5 +1,5 @@
 import networkx as nx
-
+import matplotlib.pyplot as plt
 
 class LoadFromFile(object):
     def __init__(self):
@@ -8,7 +8,6 @@ class LoadFromFile(object):
         """
         self.g = nx.Graph()
 
-        pass
 
     def from_edgelist(self, path):
         '''
@@ -31,13 +30,63 @@ class LoadFromFile(object):
             for line in f:
                 node_pair = line.replace('\n', '').split(' ')
                 edgelist += [node_pair]
+
         self.g.add_edges_from(edgelist)
-        print(nx.info(self.g))
-        print('Edgelist txt data successfully loaded into a networkx Graph!')
         return self.g
 
-    def from_in_class_network(self):  # This is Prob. 3-a.
+    def from_in_class_network(self,path):  # This is Prob. 3-a.edge
+        # This is Prob. 4-a.
         '''
-        Write your code documentation here.  # This is Prob. 4-a.
+        Read graph in edgelist txt format from 'path'.
+
+        Parameters
+        ----------
+        path:'str'
+            The path is indicated the existed file.
+
+        Returns
+        -------
+        G: 'NetworkX graph'
+            The graph generated from the file.
         '''
+        edgelist=[]
+        with open(path,'r') as file:
+            title=list(file.readline().split(' '))
+            line=file.readline()
+            while len(line)>0:
+                #read each line to create the nodes and edges
+                line=list(line.strip('\n').split('\t'))
+                target=list(line[1].split(','))
+                for i in range(len(target)):
+                    if target[i]==' ':
+                        self.g.add_node(int(line[0]))
+                        continue
+                    edgelist.extend([(int(line[0]),int(target[i]))])
+                    #append the inverse edges
+                    if (int(target[i]),int(line[0])) not in edgelist:
+                        edgelist.extend([(int(target[i]),int(line[0]))])
+                line=file.readline()
+            self.g.add_edges_from(edgelist)
         return self.g
+
+    def Avg_degree(self,G):
+        '''
+        Compute the average degree of the graph 'G'.
+
+        Parameters
+        ----------
+        G:'graph'
+            The existed graph we wnat to know.
+
+        Returns
+        -------
+        avg_degree: 'float'
+            The average degree of the graph.
+        '''
+        temp=list(G.degree())
+        sum_degree=0
+        #compute the sum of degree of each node's degree
+        for i in range(len(temp)):
+            sum_degree+=temp[i][1]
+        avg_degree=sum_degree/len(temp)
+        return avg_degree
--- networkie/utils/Measures.py
+++ networkie/utils/Measures.py
@@ -1,9 +1,108 @@
-def compute_num_triangles():  # This is Prob. 3-e.
+from Custom import LoadFromFile
+import networkx as nx
+def compute_num_triangles(G):  # This is Prob. 3-e.
     '''
-    Write your code documentation here.  # This is Prob. 4-a.
-    '''
-    return
+    # This is Prob. 4-a.
+    Compute the number of the triangles from the graph.
 
+    Parameters
+    ----------
+    G: 'NetwokX graph'
+        Graph is from the file constructing the nodes.
+
+    Returns
+    -------
+    len(friends): 'int'
+                   The number of the triangles. 
+    '''
+    peo={}
+    for i in range(len(G)):
+        peo[i]=list(G.neighbors(i))
+    friends=[]
+    for i in range(len(peo)):
+        for k in range(len(peo[i])):
+            friend=peo[i][k] #common friend
+            for j in range(len(peo[friend])):
+                if peo[friend][j] in peo[i]:
+                    common=sorted([i,friend,peo[friend][j]]) #avoid same pair
+                    if common not in friends:
+                        friends.extend([common])
+    return len(friends)
+
+def recommend_friend(G): 
+    '''
+    To build a potential friends list to each node.
+
+    Parameters
+    ----------
+    G: 'NetwokX graph'
+        The resource graph we want to construct the potential list.
+
+    Returns
+    -------
+    recommend_friend: 'dict'
+                   The list which key is the node and the content is the list 
+                    of its potential friends. 
+    '''
+    nodes=list(G.nodes())
+    recommend_friend={}
+    #create the list type storing the potential friends
+    for i in range(len(nodes)):
+        recommend_friend[i]=[]
+
+    for num in range(len(nodes)):
+        #nodes[num] is the mutual friend
+        check=list(G.neighbors(nodes[num]))
+        #check is the list of the nodes[num]'s friends
+        for i in range(len(check)):
+            peoples=list(G.neighbors(check[i]))
+            #peoples is the detected person's friends
+            for k in range(len(check)):
+                #check[k] is the person also in mutual friend's friends
+                if i==k:
+                    continue
+                if check[k] not in peoples and check[k] not in recommend_friend[check[i]]: #check[i] does not know check[k]
+                    recommend_friend[check[i]].append(check[k])
+
+    return recommend_friend
+
+def mutual_friends(G,domain_firends,re_friends): #domain_firends(list),re_friends(dict)
+    '''
+    To find the best three potential friends of NO.45
+
+    Parameter
+    ---------
+    G: 'NetworkX graph'
+        The social network.
+
+    domain_firends: 'list'
+                    The list of NO.45's friends.
+
+    re_friends: 'dict'
+                The dictionary contains NO.45's potential friends.
+
+    Returns
+    -------
+    friend: 'list'
+            The best 3 potential friends we want to recommend to NO.45.
+    '''
+    potential=re_friends[45]
+    friend=[]
+    for rank in range(3):
+        max_num,max_fri=0,-1
+        for i in range(len(potential)): #potential friend
+            temp=0
+            for j in range(len(domain_firends)): #In NO.45's friends
+                #comapre the potential person's friends and NO.45's friends' friends
+                domain_list=list(G.neighbors(domain_firends[j]))
+                for a in range(len(domain_list)):
+                    if domain_list[a] in G.neighbors(potential[i]):
+                        temp+=1
+            if temp>max_num and potential[i] not in friend:
+                max_num=temp
+                max_fri=potential[i]
+        friend.append(max_fri)
+    return friend
 
 class Node(object):
     def __init__(self):
@@ -12,9 +111,26 @@ class Node(object):
     def betweenness(self):
         pass
 
-    def degree_dist(self):  # This is Prob. 3-d.
+    def degree_dist(self, G):  # This is Prob. 3-d.
         '''
-        Write your code documentation here.  # This is Prob. 4-a.
-        '''
-        return list()
+        # This is Prob. 4-a.
+        Compute the distribution of the degree.
 
+        Parameter
+        ---------
+        G: 'NetworkX graph'
+            The graph is created from the file.
+
+        Returns
+        -------
+        degree: 'list'
+                The list of all nodes' degree.
+        '''
+        # there are some lack ID #
+        degree=[]
+        nodes=list(G.nodes())
+        for i in range(len(nodes)):
+            #degree conrains in and out degree
+            n=G.degree(nodes[i])
+            degree.append(n)
+        return degree
