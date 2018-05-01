27cf483e96880d450e8b19b96b0bd2f7b405a15e
chiapeilin <alice715610@gmail.com>
2018-05-01 18:00:46 +0800

    HW3

--- networkie/utils/Measures.py
+++ networkie/utils/Measures.py
@@ -1,7 +1,18 @@
-def compute_num_triangles():  # This is Prob. 3-e.
+def compute_num_triangles(g):  # This is Prob. 3-e.
     '''
     Write your code documentation here.  # This is Prob. 4-a.
+
+    Parameters
+    ----------
+    compute how many triangles does each node has and sum of it ,then devide 3 
+
+    Returns
+    -------
+    g: `NetworkX graph`
     '''
+    tri = nx.triangles(g)   
+    return print("number of triangles:",sum(tri.values())/3)
+
     return
 
 
@@ -13,8 +24,23 @@ class Node(object):
         pass
 
     def degree_dist(self):  # This is Prob. 3-d.
+
         '''
         Write your code documentation here.  # This is Prob. 4-a.
+
+        Parameters
+        ----------
+        put degrees into list
+        draw a graph
+
+        Returns
+        -------
+        g: `NetworkX graph`
         '''
-        return list()
+        l=[]
+        for i in range(len(g.nodes())):
+            l.append(g.degree(i))
+            
+        return l
+
 
